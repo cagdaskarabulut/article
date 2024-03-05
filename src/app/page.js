54a4f9e1823e52cd9c678544ef11c0fb6b888443
cagdaskarabulut <cagdas.karabulut@gmail.com>
2024-03-05 13:14:14 +0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,13 @@ import ScrollToTopButton from "../components/reusableComponents/ScrollToTopButto
 export default function Home() {
   return (
     <>
-      <MetaPanel
+      {/* <MetaPanel
         title="NewsZipped - Read Articles and News on Various Topics"
         descriptionContent="NewsZipped is an online platform offering up-to-date articles and news on various topics. Stay informed with our diverse range of content."
         keywordsContent="articles, news, NewsZipped, online platform, current affairs, diverse topics"
         imagePath="/images/icon.ico"
         imageAlt="newszipped"
-      />
+      /> */}
       <HomePagePanel />
       <ScrollToTopButton showBelow={250} />
       <style jsx global>{`
