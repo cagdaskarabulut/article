e4a021a446d990768d701c08c989d0c2000c8bc3
cagdaskarabulut <cagdas.karabulut@gmail.com>
2024-04-06 10:46:29 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,6 @@ export default async function Home({ searchParams }) {
         <Container maxWidth="lg">
           
           <MyGrid
-            isHideRightSideOnMobile={true}
             leftContent={
               <>
                 <h4
