5a2f099aae1afc69d002cfb0f4fbcee03beb7ab8
cagdaskarabulut <cagdas.karabulut@gmail.com>
2024-04-10 00:32:13 +0300

    .

--- src/app/api/article/list_url/route.js
+++ src/app/api/article/list_url/route.js
@@ -3,6 +3,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const article_url_list =
-    await sql`SELECT url FROM public.newszipped_article;`;
+    await sql`SELECT url FROM public.newszipped_article where is_active=true;`;
   return NextResponse.json({ article_url_list });
 }
