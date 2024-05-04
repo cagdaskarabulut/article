5a13492ff4816b705a40fe2e32dfe61bb60f14ec
cagdaskarabulut <cagdas.karabulut@gmail.com>
2024-05-04 23:34:47 +0300

    .

--- scripts/generate-robots-txt.js
+++ scripts/generate-robots-txt.js
@@ -66,6 +66,65 @@ Sitemap: ${websiteUrl}/sitemap.xml`;
   return result;
 }
 
+function generateFinalColorsScssFile() {
+//   let result = `
+// @import "../styles/colors.scss";
+// @import "../styles/mixins.scss";
+
+// body {
+//   background-color: $containerBackgroundDesktop !important;
+// }
+// `;
+let result = `
+$color0: rgb(255, 255, 255);
+$color1: rgb(0, 0, 0);
+$color2: rgba(0, 0, 0, 0.6);
+$color3: rgb(0, 147, 233);
+$color4: rgb(211, 70, 0);
+$color5: rgb(220, 220, 220);
+$color6: #2F7D31;
+$color7: rgb(230, 244, 231);
+$color8: rgb(242, 242, 242);
+
+
+$PanelContainerColor: $color0;
+$containerBackgroundDesktop: $color8;
+$containerBackgroundHeader: $color8;
+$background: $color8;
+$font: $color3;
+$textFieldErrorBorderColor: rgb(255, 0, 0);
+
+$firstButtonFontColor: $color1;
+$firstButtonBackgroundColor: $color7;
+$firstButtonBorderColor: $color6;
+$firstButtonFontColorHover: $color0;
+
+$secondButtonFontColor: $color0;
+$secondButtonBackgroundColor: $color6;
+$secondButtonBorderColor: $color7;
+
+$thirdButtonFontColor: $color0;
+$thirdButtonBackgroundColor: $color4;
+$thirdButtonBorderColor: $color0;
+
+$fourthButtonFontColor: $color0;
+$fourthButtonBackgroundColor: $color3 ;
+$fourthButtonBorderColor: $color3 ;
+
+`;
+  return result;
+}
+
+function generateFinalGlobalsScssFile() {
+    let result = `
+  @import "../styles/colors.scss";
+  @import "../styles/mixins.scss";
+  `;
+    return result;
+  }
+  
+
+
 function generateFinalSitemapXmlFile(SitemapXmlFileSource) {
   let result = `
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
@@ -109,11 +168,29 @@ daily
   return result;
 }
 
-function generateRobotsTxtAndSitemapXml() {
-  if (isLocal == "false") {
+async function generateRobotsTxtAndSitemapXml() {
+  if (isLocal == "true") {
+    // let colorsScss = generateFinalColorsScssFile();
+    //     let globalsScss = generateFinalGlobalsScssFile();
+    // fs.writeFileSync("src/styles/colors.scss", colorsScss);
+    //     fs.writeFileSync("src/app/globals.scss", globalsScss);
+
+    await fetch(process.env.URL + "/api/article/article_project_auto_generate_files", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((dataList) => {
+        //- add auto generated urls
+        dataList?.file?.rows.map((activeFile, index) => {
+          if(activeFile?.project_name == siteName){
+            fs.writeFileSync(activeFile?.file_path, activeFile?.file_content);
+          }
+        });
+      });
+
     let dynamicRobotsTxtFields = "";
     let dynamicSitemapFields = addStaticValuesIntoSitemapList();
-    fetch(process.env.URL + "/api/article/list_url", {
+    await fetch(process.env.URL + "/api/article/list_url", {
       method: "GET",
     })
       .then((res) => res.json())
@@ -133,24 +210,15 @@ function generateRobotsTxtAndSitemapXml() {
         //-generate final files to store
         let robotsTxt = generateFinalRobotsTxtFile(dynamicRobotsTxtFields);
         let sitemapXml = generateFinalSitemapXmlFile(dynamicSitemapFields);
+        
 
         //-create physical files
         fs.writeFileSync("public/robots.txt", robotsTxt);
         fs.writeFileSync("public/sitemap.xml", sitemapXml);
+        
       });
 
-      fetch(process.env.URL + "/api/article/article_project_auto_generate_files", {
-        method: "GET",
-      })
-        .then((res) => res.json())
-        .then((dataList) => {
-          //- add auto generated urls
-          dataList?.file?.rows.map((activeFile, index) => {
-            if(activeFile?.project_name == siteName){
-              fs.writeFileSync(activeFile?.file_path, activeFile?.file_content);
-            }
-          });
-        });
+      
 
 
 
--- src/components/reusableComponents/CommentItem.js
+++ src/components/reusableComponents/CommentItem.js
@@ -1,13 +1,15 @@
 "use client";
 import { Divider } from "@mui/material";
 import { format } from "date-fns";
+import styles from "./CommentItem.module.scss";
+
 const CommentItem = ({ comment, isMobile }) => {
   return (
     <>
         <br />
         <b>{comment?.user_name}</b>
         {isMobile ? (<br />) : (<span> - </span>)}
-        <span style={{ color: "rgba(0, 0, 0, 0.6)" }}>
+        <span className={styles.SpanStyle}>
           {format(comment?.create_date, "dd/MM/yyyy hh:mm")}
         </span>
         <p dangerouslySetInnerHTML={{ __html: comment?.comment }}></p>
