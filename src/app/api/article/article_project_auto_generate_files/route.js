fea2892501685ae7822222ad8a8d2e3d34816f79
cagdaskarabulut <cagdas.karabulut@gmail.com>
2024-05-07 00:20:49 +0300

    .

--- src/app/api/article/article_project_auto_generate_files/route.js
+++ src/app/api/article/article_project_auto_generate_files/route.js
@@ -4,9 +4,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
   let file;
   if (process.env.PROJECT_SITE_NAME === "newszipped") {
-    file = await sql`SELECT id, file_name, file_content, file_path, project FROM public.article_project_auto_generate_files where project='newszipped';`;
+    file = await sql`SELECT id, file_name, file_content, file_path, project FROM public.article_project_auto_generate_files where project='newszipped' and is_passive='false';`;
   } else if (process.env.PROJECT_SITE_NAME === "brickstanbul") {
-    file = await sql`SELECT id, file_name, file_content, file_path, project FROM public.article_project_auto_generate_files where project='brickstanbul';`;
+    file = await sql`SELECT id, file_name, file_content, file_path, project FROM public.article_project_auto_generate_files where project='brickstanbul' and is_passive='false';`;
   }
   return NextResponse.json({ file });
 }
