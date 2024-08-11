a439517d748d6e134bbf1d329bf14815deabf0e8
Cagdas Karabulut <cagdas.karabulut@gmail.com>
2024-08-11 22:48:15 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,8 @@ import FocusContent from "../components/reusableComponents/FocusContent";
 import { LABELS as LABELS_en } from "./enums/lang/en";
 import { LABELS as LABELS_tr } from "./enums/lang/tr";
 
-export const dynamicParams = true; // true | false,
-//export const revalidate = 86400;
-export const revalidate = 60;
+export const dynamicParams = true; // true | 
+export const revalidate = 86400;
 
 export default async function Home({ searchParams }) {
   const specialFields = await fetch(
