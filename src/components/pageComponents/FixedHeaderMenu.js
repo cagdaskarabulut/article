0444ab579d01c6c3099a3ab917ad92e7a703808f
cagdaskarabulut <cagdas.karabulut@gmail.com>
2024-09-04 16:43:08 +0300

    iletişim ve e-sinav eklendi

--- src/components/pageComponents/FixedHeaderMenu.js
+++ src/components/pageComponents/FixedHeaderMenu.js
@@ -1,140 +1,3 @@
-// "use client";
-// import React, { useState, useEffect } from "react";
-// import MenuIcon from "@mui/icons-material/Menu";
-// import {
-//   IconButton,
-//   Drawer,
-//   List,
-//   ListItem,
-//   ListItemText,
-//   Box,
-// } from "@mui/material";
-// import Image from "next/image";
-// import styles from "./FixedHeaderMenu.module.scss";
-
-// const FixedHeaderMenu = () => {
-//   const [isMobile, setIsMobile] = useState(false);
-//   let iconHref = `https://karabulut-storage.s3.amazonaws.com/${process.env.PROJECT_SITE_NAME}/logo.png`;
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setIsMobile(window.innerWidth <= 768);
-//     };
-
-//     handleResize();
-//     window.addEventListener("resize", handleResize);
-
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-//   }, []);
-
-//   const [drawerOpen, setDrawerOpen] = useState(false);
-
-//   const toggleDrawer = (open) => () => {
-//     setDrawerOpen(open);
-//   };
-
-//   const menuItems = (
-//     <List>
-//       <ListItem button component="a" href="#">
-//         <ListItemText primary="Anasayfa" />
-//       </ListItem>
-//       <ListItem button component="a" href="e-sinav-deneme">
-//         <ListItemText primary="E-Sınav Deneme" />
-//       </ListItem>
-//       <ListItem button component="a" href="#">
-//         <ListItemText primary="Hakkımızda" />
-//       </ListItem>
-//       <ListItem button component="a" href="#">
-//         <ListItemText primary="Ekibimiz" />
-//       </ListItem>
-//       <ListItem button component="a" href="#">
-//         <ListItemText primary="Galeri" />
-//       </ListItem>
-//       <ListItem button component="a" href="#">
-//         <ListItemText primary="İletişim" />
-//       </ListItem>
-//     </List>
-//   );
-
-//   return (
-//     <header className={styles.header}>
-//       <nav className={styles.nav}>
-//         <div className={styles.logoContainer}>
-//           <a href="/">
-//             <Image src={iconHref} alt="Logo" width={40} height={40} />
-//           </a>
-//         </div>
-//         {!isMobile && (
-//           <ul className={styles.navList}>
-//             <li className={styles.navItem}>
-//               <a href="#" className={styles.navLink}>
-//                 Anasayfa
-//               </a>
-//             </li>
-//             <li className={styles.navItem}>
-//               <a href="e-sinav-deneme" className={styles.navLink}>
-//                 E-Sınav Deneme
-//               </a>
-//             </li>
-//             <li className={styles.navItem}>
-//               <a href="#" className={styles.navLink}>
-//                 Hakkımızda
-//               </a>
-//             </li>
-//             <li className={styles.navItem}>
-//               <a href="#" className={styles.navLink}>
-//                 Ekibimiz
-//               </a>
-//             </li>
-//             <li className={styles.navItem}>
-//               <a href="#" className={styles.navLink}>
-//                 Galeri
-//               </a>
-//             </li>
-//             <li className={styles.navItem}>
-//               <a href="#" className={styles.navLink}>
-//                 İletişim
-//               </a>
-//             </li>
-//           </ul>
-//         )}
-//         {isMobile && (
-//           <IconButton
-//             edge="end"
-//             color="inherit"
-//             aria-label="menu"
-//             onClick={toggleDrawer(true)}
-//             className={styles.menuButton}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//         )}
-//       </nav>
-
-//       <Drawer
-//         anchor="right"
-//         open={drawerOpen}
-//         onClose={toggleDrawer(false)}
-//         classes={{ paper: styles.drawer }} // Applying the custom drawer class
-//       >
-//         <Box
-//           sx={{ width: 250 }}
-//           role="presentation"
-//           onClick={toggleDrawer(false)}
-//           onKeyDown={toggleDrawer(false)}
-//           className={styles.drawer}
-//         >
-//           {menuItems}
-//         </Box>
-//       </Drawer>
-//     </header>
-//   );
-// };
-
-// export default FixedHeaderMenu;
-
 "use client";
 import React, { useState, useEffect } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -185,7 +48,11 @@ const FixedHeaderMenu = () => {
       <ListItem button component="a" href="#">
         <ListItemText primary="Anasayfa" />
       </ListItem>
-      <ListItem button component="a" href="e-sinav-deneme">
+      <ListItem
+        button
+        component="a"
+        href="https://kolayehliyet.com/bafra-ozkan-surucu-kursu/test-coz"
+      >
         <ListItemText primary="E-Sınav Deneme" />
       </ListItem>
       <ListItem button component="a" href="#">
@@ -197,7 +64,7 @@ const FixedHeaderMenu = () => {
       <ListItem button component="a" href="#">
         <ListItemText primary="Galeri" />
       </ListItem>
-      <ListItem button component="a" href="#">
+      <ListItem button component="a" href="iletisim">
         <ListItemText primary="İletişim" />
       </ListItem>
     </List>
@@ -228,7 +95,10 @@ const FixedHeaderMenu = () => {
               </a>
             </li>
             <li className={styles.navItem}>
-              <a href="e-sinav-deneme" className={styles.navLink}>
+              <a
+                href="https://kolayehliyet.com/bafra-ozkan-surucu-kursu/test-coz"
+                className={styles.navLink}
+              >
                 E-Sınav Deneme
               </a>
             </li>
@@ -248,7 +118,7 @@ const FixedHeaderMenu = () => {
               </a>
             </li>
             <li className={styles.navItem}>
-              <a href="#" className={styles.navLink}>
+              <a href="iletisim" className={styles.navLink}>
                 İletişim
               </a>
             </li>
