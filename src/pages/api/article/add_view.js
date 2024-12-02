b5f65e2c7d987c58d77d08f85dbe9936618d9cc3
Çağdaş Yıldırım Karabulut <cagdas@192.168.0.143>
2024-12-02 11:01:02 +0300

    apilerde cors kontrolü

--- src/pages/api/article/add_view.js
+++ src/pages/api/article/add_view.js
@@ -1,6 +1,34 @@
 import { sql } from "@vercel/postgres";
 
 export default async function handler(request, response) {
+  const localAddress = process.env.URL;
+  const websiteAddress = process.env.URL_WEBSITE;
+
+  const fullOrigin =
+    request.headers.origin ||
+    request.headers.referer ||
+    `${request.headers["x-forwarded-proto"]}://${request.headers["x-forwarded-host"]}` ||
+    "http://localhost:3000"; // Varsayılan değer
+
+  // Ana URL'yi almak için URL API'sini kullanıyoruz
+  let origin;
+  try {
+    origin = new URL(fullOrigin).origin; // Sadece kök URL alınır
+  } catch (error) {
+    console.error("Invalid Origin:", fullOrigin);
+    return response.status(400).json({ error: "Invalid origin header" });
+  }
+
+  console.log("Detected Origin:", origin);
+
+  const isDevelopment = process.env.NODE_ENV === "development";
+  if (
+    !isDevelopment &&
+    (!origin || (origin !== localAddress && origin !== websiteAddress))
+  ) {
+    return response.status(403).json({ error: "Unauthorized origin" });
+  }
+
   const projectName = process.env.PROJECT_SITE_NAME;
   try {
     const values = [
