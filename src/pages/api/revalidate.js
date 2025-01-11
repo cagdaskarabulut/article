ef5af00dfcbd762d62ed2ca73b887681763faa90
Çağdaş Yıldırım Karabulut <cagdas@192.168.0.101>
2025-01-11 12:04:22 +0300

    isr anasayfa için revalidate eklendi

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,3 +1,22 @@
+// export default async function handler(req, res) {
+//   const { path } = req.query; // Hangi sayfanın yeniden oluşturulacağını alıyoruz
+
+//   if (!path) {
+//     return res.status(400).json({ message: "Path parametresi eksik" });
+//   }
+
+//   try {
+//     // Path'i yeniden oluştur
+//     // curl -X GET "https://www.cnmautoparts.com/api/revalidate?path=i%CC%87leti%C5%9Fim"
+//     // curl -X GET "https://www.cnmautoparts.com/api/revalidate?path=isuzu-disli-kutu-kapak-conta-4jj1-4jk1-939686063360"
+//     await res.revalidate(`/${decodeURIComponent(path)}`); // Path'i çözerek ISR'yi tetikle
+//     return res.json({ revalidated: true });
+//   } catch (err) {
+//     console.error("Yeniden oluşturma hatası:", err);
+//     return res.status(500).send("Yeniden oluşturma başarısız");
+//   }
+// }
+
 export default async function handler(req, res) {
   const { path } = req.query; // Hangi sayfanın yeniden oluşturulacağını alıyoruz
 
@@ -7,9 +26,20 @@ export default async function handler(req, res) {
 
   try {
     // Path'i yeniden oluştur
-    // curl -X GET "https://www.cnmautoparts.com/api/revalidate?path=i%CC%87leti%C5%9Fim"
-    // curl -X GET "https://www.cnmautoparts.com/api/revalidate?path=isuzu-disli-kutu-kapak-conta-4jj1-4jk1-939686063360"
-    await res.revalidate(`/${decodeURIComponent(path)}`); // Path'i çözerek ISR'yi tetikle
+    // curl -X GET "https://www.cnmautoparts.com/api/revalidate?path=homepage"
+    // curl -X GET "https://www.cnmautoparts.com/api/revalidate?path=route-isuzu-disli-kutu-kapak-conta-4jj1-4jk1-939686063360"
+    if (path === "homepage") {
+      // Ana sayfa için yeniden oluşturma
+      await res.revalidate(`/`);
+    } else if (path.startsWith("route")) {
+      // Belirli bir route için yeniden oluşturma
+      const specificRoute = path.replace("route-", ""); // Örneğin: route-about -> /about
+      await res.revalidate(`/${decodeURIComponent(specificRoute)}`);
+    } else {
+      // Dinamik sayfalar için yeniden oluşturma
+      await res.revalidate(`/${decodeURIComponent(path)}`);
+    }
+
     return res.json({ revalidated: true });
   } catch (err) {
     console.error("Yeniden oluşturma hatası:", err);
