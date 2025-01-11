25832171570f1e8732b87cd67ef47cf9ebdd75cd
Çağdaş Yıldırım Karabulut <cagdas@192.168.0.101>
2025-01-11 12:20:43 +0300

    isr anasayfa için revalidate eklendi

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,22 +1,3 @@
-// export default async function handler(req, res) {
-//   const { path } = req.query; // Hangi sayfanın yeniden oluşturulacağını alıyoruz
-
-//   if (!path) {
-//     return res.status(400).json({ message: "Path parametresi eksik" });
-//   }
-
-//   try {
-//     // Path'i yeniden oluştur
-//     // curl -X GET "https://www.cnmautoparts.com/api/revalidate?path=i%CC%87leti%C5%9Fim"
-//     // curl -X GET "https://www.cnmautoparts.com/api/revalidate?path=isuzu-disli-kutu-kapak-conta-4jj1-4jk1-939686063360"
-//     await res.revalidate(`/${decodeURIComponent(path)}`); // Path'i çözerek ISR'yi tetikle
-//     return res.json({ revalidated: true });
-//   } catch (err) {
-//     console.error("Yeniden oluşturma hatası:", err);
-//     return res.status(500).send("Yeniden oluşturma başarısız");
-//   }
-// }
-
 export default async function handler(req, res) {
   const { path } = req.query; // Hangi sayfanın yeniden oluşturulacağını alıyoruz
 
@@ -25,18 +6,12 @@ export default async function handler(req, res) {
   }
 
   try {
-    // Path'i yeniden oluştur
-    // curl -X GET "https://www.cnmautoparts.com/api/revalidate?path=homepage"
-    // curl -X GET "https://www.cnmautoparts.com/api/revalidate?path=route-isuzu-disli-kutu-kapak-conta-4jj1-4jk1-939686063360"
     if (path === "homepage") {
-      // Ana sayfa için yeniden oluşturma
+      console.log("Ana sayfa yeniden oluşturuluyor...");
       await res.revalidate(`/`);
-    } else if (path.startsWith("route")) {
-      // Belirli bir route için yeniden oluşturma
-      const specificRoute = path.replace("route-", ""); // Örneğin: route-about -> /about
-      await res.revalidate(`/${decodeURIComponent(specificRoute)}`);
+      console.log("Ana sayfa başarıyla yeniden oluşturuldu.");
     } else {
-      // Dinamik sayfalar için yeniden oluşturma
+      // Diğer sayfalar için yeniden oluşturma
       await res.revalidate(`/${decodeURIComponent(path)}`);
     }
 
